b839f27f19f3a2e0561cc8efb41efbb9379473a3
Thane Brimhall <thane.brimhall@gmail.com>
2015-06-19 22:50:31 -0600

    Progress on a standardized channel schema

--- util.py
+++ util.py
@@ -1,25 +1,52 @@
+"""
+All internal message types:
+
+NAME:           CHANNEL:                        PAYLOAD:
+User Join       {ZONE_ID}.join                  {USER_ID}
+User Leave?     {ZONE_ID}.leave                 {USER_ID}
+Public create   {ZONE_ID}.create.{DO_CLASS}     {SERIALIZED_DO}
+Public update   {ZONE_ID}.update                {SERIALIZED_DO}
+Public delete   {ZONE_ID}.delete                {SERIALIZED_DO}
+Public call     {ZONE_ID}.call.{DO_METHOD}      {SERIALIZED_ARGUMENTS}
+Whisper         {USER_ID}.*                     (same as public)
+Custom          {WHATEVER}                      {WHATEVER}
+
+Note that Zone IDs are serialized in the DO anyway, so the duplication of the
+ZONE_ID in the channel serves entirely as an internal message pruning system.
+"""
+# TODO: Should we mark the whispers specially, since they are unique?
+# TODO: Should this class move into an internal_messaging module with Redis?
 
 
-class Channel:
-    def __init__(self, *, target_zone=None, object_class=None, method=None,
-                 target_user=None):
-        """For example"""
-        # TODO: Aren't target zone and user the same thing?
-        self.target_zone = target_zone
-        self.object_class = object_class
+class InternalChannel:
+    """A classy representation of channels. This makes it easier
+    to route messages cleanly.
+    """
+    def __init__(self, *, target: str=None, method=None, code_name=None):
+        """
+        :param target: Either the zone or the user ID.
+        :param method: Used to determine what is taking place
+        :param code_name: Only used in "create" channels (to pick a class
+        from the registry) and in "call" channels (to pick the method that
+        should be executed)
+        """
+        self.target = target
         self.method = method
-        self.target_user = target_user
+        self.code_name = code_name
+        if self.code_name and self.method not in ["create", "call"]:
+            raise TypeError("")
 
     @staticmethod
-    def parse(channel_expression):
+    def parse(channel_expression: str):
         """Parses the channel name into a usable description."""
-        components = channel_expression.split('.')
-        # TODO: If the first piece is a UUID, it's the target user
-        # TODO: The zone name
-        # TODO: The object class
-        # TODO: The method name
+        target, method, *rest = channel_expression.split('.')
+        code = None
+        if rest:
+            code = rest
+        return InternalChannel(target=target, method=method, code_name=code)
 
     def __str__(self):
-        # TODO: Render this to the dotted notation
-        return "{}.{}.{}.{}".format(
-            self.target_user, self.target_zone, self.object_class, self.method)
+        pieces = [self.target, self.method]
+        if self.code_name:
+            pieces.append(self.code_name)
+        return ".".join(self.code_name)
